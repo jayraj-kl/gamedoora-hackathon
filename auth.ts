2ade654e6a79c3b71d1e9b1c9993bfbc1cffb29b
Jayraj Ladkat <jayrajkladkat@gmail.com>
2025-02-01 23:56:00 +0530

    added next-auth

--- auth.ts
+++ auth.ts
@@ -1,5 +1,12 @@
 import NextAuth from "next-auth"
+import Google from "next-auth/providers/google"
+
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import { prisma } from "@/prisma"
  
-export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  // debug: !!process.env.AUTH_DEBUG,
+  // theme: { logo: "https://authjs.dev/img/logo-sm.png" },
+  adapter: PrismaAdapter(prisma),
+  providers: [ Google ],
 })
